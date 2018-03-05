d840eb4089060550090a169c5bcf0ccc722c82fb
Kai Chan <kaichan39@outlook.com>
2018-03-05 15:57:58 +0000

    Initial commit

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * This script is used to rotate the imported skybox to give it a more dynamic feel and look
+ * Code referenced from https://www.youtube.com/watch?v=cqGq__JjhMM
+ */
+public class RotateSky : MonoBehaviour {
+
+    public float rotationSpeed;
+	// Update is called once per frame
+
+	void FixedUpdate () {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+	}
+}
